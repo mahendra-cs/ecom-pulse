f3650f365584c53af8ab031c677f1d2590221ea7
Mahendra Chaurasia <mahendracs@gmail.com>
2025-08-17 22:06:33 +0530

    feat: Add cloud-init deployment script

---
+++ cloud-init-deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Clone the repository
+git clone https://github.com/mahendra-cs/ecom-pulse.git
+cd ecom-pulse
+
+# Build and run the services
+docker-compose up --build -d
